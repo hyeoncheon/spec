52d02e7ff44a1803d1aee50b93b244e1d30be9e1
Yonghwan SO <scinix@gmail.com>
2018-04-20 03:26:06 +0900

    initial version of honcheonui-spec

---
+++ README.md
@@ -0,0 +1,35 @@
+# Honcheonui SPEC
+
+This simple package, spec, provides interface specifications for honcheonui
+project. Currently it contains plugin specification include interface for
+provider plugin and data types for that plugins.
+
+This interface and struct definitions were originally on the
+[Honcheonui Project](https://github.com/hyeoncheon/honcheonui)
+but now separated into here since there are some conflict when using
+`dep` and writing plugins.
+
+Anyway, Please visit
+[Honcheonui Project](https://github.com/hyeoncheon/honcheonui)
+for more information.
+
+## Author
+
+Yonghwan SO https://github.com/sio4 & http://www.sauru.so
+
+## Copyright (GNU General Public License v3.0)
+
+Copyright 2016 Yonghwan SO
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation; either version 3 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program; if not, write to the Free Software Foundation, Inc., 51
+Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
---
+++ plugin.go
@@ -0,0 +1,64 @@
+// Package spec contains struct and interface specification for honcheonui plugins
+package spec
+
+import (
+	"time"
+
+	"github.com/gobuffalo/uuid"
+)
+
+// HoncheonuiResource is a struct for containing resource data
+type HoncheonuiResource struct {
+	Provider           string
+	Type               string
+	OriginalID         string
+	UUID               uuid.UUID
+	Name               string
+	Notes              string
+	GroupID            string
+	ResourceCreatedAt  time.Time
+	ResourceModifiedAt time.Time
+	IPAddress          string
+	Location           string
+	IsConn             bool
+	IsOn               bool
+	Attributes         map[string]string
+	IntegerAttributes  map[string]int
+	Tags               []string
+	UserIDs            []string
+	Raw                interface{}
+}
+
+// HoncheonuiStatus is a struct for containing status data
+type HoncheonuiStatus struct {
+	OriginalID string
+	IsConn     bool
+	IsOn       bool
+}
+
+// HoncheonuiNotification is a struct for containing notification data
+type HoncheonuiNotification struct {
+	Provider    string
+	Type        string
+	OriginalID  string
+	GroupID     string
+	UserID      string
+	Title       string
+	Content     string
+	Category    string
+	IssuedBy    string
+	IsOpen      bool
+	IssuedAt    time.Time
+	ModifiedAt  time.Time
+	ResourceIDs []string
+	UserIDs     []string
+}
+
+// Provider is an interface for provider plugins
+type Provider interface {
+	Init() error
+	CheckAccount(string, string) (int, int, error)
+	GetResources(string, string) ([]interface{}, error)
+	GetStatuses(string, string) ([]interface{}, error)
+	GetNotifications(string, string, time.Time) ([]interface{}, error)
+}
